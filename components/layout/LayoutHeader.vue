1756074c084e78fdbb369841fff69993f1a40d7a
Samuel <samuel.cflau@gmail.com>
2022-12-16 01:00:00 +0800

    Nav bar - minor update

--- components/layout/LayoutHeader.vue
+++ components/layout/LayoutHeader.vue
@@ -1,13 +1,17 @@
 <template>
 <div class="layout-header">
-    <div class="header-logo">
-    <nuxt-link to="/">
-        <img class="logo-image" :src="require('@/assets/img/Logo.png')" />
-    </nuxt-link>
+  <div class="header-logo">
+      <nuxt-link to="/">
+          <img class="logo-image" :src="require('@/assets/img/Logo.png')" />
+      </nuxt-link>
     </div>
-    <el-menu :default-active="activeIndex" class="header-menu" mode="horizontal" @select="handleSelect">
-    <el-menu-item index="about"><nuxt-link to="/about">About</nuxt-link></el-menu-item>
-    <el-menu-item index="blog"><nuxt-link to="/blog">Blog</nuxt-link></el-menu-item>
+  <el-menu :default-active="activeIndex" class="header-menu" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="about">
+            <nuxt-link to="/about">About</nuxt-link>
+        </el-menu-item>
+        <el-menu-item index="blog">
+            <nuxt-link to="/blog">Blog</nuxt-link>
+        </el-menu-item>
     </el-menu>
     <div class="header-social-media-icons">
     </div>
@@ -41,16 +45,29 @@ export default {
         max-width: 40px;
         margin-bottom: 0px;
     }
-}
+    .header-menu {
+        ::v-deep.el-menu.el-menu--horizontal {
+            border-bottom: none;
 
-.header-menu {
-    ::v-deep.el-menu.el-menu--horizontal {
-        border-bottom: none;
-    }
+            .el-menu-item,
+            .el-menu-item.is-active {
+                border-bottom: none;
+            }
+        }
 
-    ::v-deep .el-menu-item {
-        font-size: 18px;
-        font-weight: bold;
+        ::v-deep .el-menu-item {
+            font-size: 18px;
+            font-weight: bold;
+        }
+
+        a {
+            color: black;
+
+            &:hover {
+                color: gray;
+                text-decoration: none;
+            }
+        }
     }
 }
 </style>
