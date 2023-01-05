a6a2554a986ac4363cc8eca0b57167fc9041ab2d
Samuel <samuel.cflau@gmail.com>
2023-01-06 01:06:52 +0800

    Finalize "About" page

--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="container">
+  <div class="container mb-5">
     <div class="row mt-1 gap-2">
       <div class="about-section-content col-12-xs col-6-sm col-6-xl">
         <div class="about-section-title font-xl mb-2">ABOUT ME</div>
-        <p class="mb-2">It’s Samuel, Front-End Web Developer. After graduated with a degree in commerce, I
-          started to work as a rehab fitness trainer in fitness industry. I enjoy the process to help people
-          achieving their fitness goals.</p>
-        <p class="mb-2">Like many others, the coronavirus (COVID-19) outbreak has had a big impact on my career,
-          which I had started to develop an interest in graphic design and using WordPress to develop my first
-          website.</p>
-        <p class="mb-2">Learning to code and web design through a self-learning path is not easy. However, it’s
-          the creative work that makes me happy and I enjoy bringing ideas to life through browser. It’s my
-          pleasure to share it with others!</p>
+        <div v-for="(paragraph, index) in paragraphContent" :key="index">
+          <p class="mb-2">{{ paragraph.content }}</p>
+        </div>
       </div>
-      <div class="about-section-imag col-12-xs col-6-sm col-6-xl">
+      <div class="about-section-image col-12-xs col-6-sm col-6-xl">
         <img class="logo-image" :src="require('@/assets/img/portrait.png')" />
       </div>
     </div>
+    <el-divider />
     <div class="row mt-1 gap-2">
       <div class="experience-section-content col-12-xs col-6-sm col-6-xl">
         <div class="experience-section-title font-xl mb-2">EXPERIENCE</div>
@@ -32,10 +27,10 @@
       <div class="skill-section-content col-12-xs col-6-sm col-6-xl">
         <div class="skill-section-title font-xl mb-2">SKILLS</div>
         <div class="progress-bar p-2 br-sm">
-          <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
-          <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
-          <el-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></el-progress>
-          <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
+          <div v-for="(item, index) in progressBarData" :key="index">
+            <span class="skill-title">{{ item.skillTitle }}</span>
+            <el-progress :text-inside="false" :stroke-width="item.strokeWidth" :percentage="item.percentage" />
+          </div>
         </div>
       </div>
     </div>
@@ -46,64 +41,154 @@
 export default {
   name: "About",
   data() {
+    const paragraphContent = [
+      {
+        content: `It’s Samuel, Front-End Web Developer. After graduated with a degree in commerce, I
+          started to work as a rehab fitness trainer in fitness industry. I enjoy the process to help people
+          achieving their fitness goals.`,
+      },
+      {
+        content: `Like many others, the coronavirus (COVID-19) outbreak has had a big impact on my career,
+          which I had started to develop an interest in graphic design and using WordPress to develop my first
+          website.`,
+      },
+      {
+        content: `Learning to code and web design through a self-learning path is not easy. However, it’s
+          the creative work that makes me happy and I enjoy bringing ideas to life through browser. It’s my
+          pleasure to share it with others!`,
+      }
+    ]
     const activities = [
-          {
-            title: 'Web Developer',
-            company: 'PressLogic',
-            timestamp: '2022-current',
-            type: 'primary',
-            size: 'large',
-          }, {
-            title: 'Junior Web Developer',
-            company: 'LANE EIGHT',
-            timestamp: '2022',
-          }, {
-            title: 'Front-End Web Developer',
-            company: 'Freelance',
-            timestamp: '2021-2022',
-          }, {
-            title: 'Fitness Trainer',
-            company: 'Chinese YMCA of Hong Kong',
-            timestamp: '2020-2021',
-          }, {
-            title: 'Omni Sport Leader',
-            company: 'Decathlon Hong Kong',
-            timestamp: '2017-2018'
-          }
-        ]
+      {
+        title: 'Web Developer',
+        company: 'PressLogic',
+        timestamp: '2022-current',
+        type: 'primary',
+        size: 'large',
+      }, {
+        title: 'Junior Web Developer',
+        company: 'LANE EIGHT',
+        timestamp: '2022',
+      }, {
+        title: 'Front-End Web Developer',
+        company: 'Freelance',
+        timestamp: '2021-2022',
+      }, {
+        title: 'Fitness Trainer',
+        company: 'Chinese YMCA of Hong Kong',
+        timestamp: '2020-2021',
+      }, {
+        title: 'Omni Sport Leader',
+        company: 'Decathlon Hong Kong',
+        timestamp: '2017-2018'
+      }
+    ]
 
-      return { activities };
-    }
+    const progressBarData = [
+      {
+        skillTitle: 'HTML5 + CSS3',
+        strokeWidth: 26,
+        percentage: 90
+      }, {
+        skillTitle: 'JavaScript',
+        strokeWidth: 24,
+        percentage: 80
+      }, {
+        skillTitle: 'Nuxt.js',
+        strokeWidth: 22,
+        percentage: 70
+      }, {
+        skillTitle: 'Vue.js',
+        strokeWidth: 20,
+        percentage: 70
+      }, {
+        skillTitle: 'Node.js',
+        strokeWidth: 20,
+        percentage: 40
+      }, {
+        skillTitle: 'PhotoShop & Illustrator',
+        strokeWidth: 20,
+        percentage: 30
+      }
+    ]
+
+    return { paragraphContent, activities, progressBarData };
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .about-section-content {
+  box-shadow: 0 2px 12px 0 rgb(0 0 0 / 100%);
+  border-radius: 20px;
+
   p {
     line-height: 1.5rem;
   }
 }
 
+.logo-image {
+  border-radius: 20px;
+}
+
+.el-divider {
+  background-color: rgba(255, 255, 255, 0.2);
+  max-width: 100%;
+  margin: 40px 0;
+}
+
 .experience-section-content {
   .timeline {
     display: block;
-    background: rgba(255,255,255, 0.1);
+    background: rgba(255, 255, 255, 0.1);
 
-    .job-company, .job-date {
+    .job-company,
+    .job-date {
       display: block;
       padding-top: 0.5rem;
-      color: rgba(255,255,255,0.5);
+      color: rgba(255, 255, 255, 0.5);
     }
 
-    ::v-deep .el-timeline-item__content{
+    ::v-deep .el-timeline-item__content {
       color: white;
     }
   }
 }
 
 .skill-section-content {
+  box-shadow: 0 2px 12px 0 rgb(0 0 0 / 100%);
+  border-radius: 20px;
+  height: 550px;
+
   .progress-bar {
-    background: rgba(255,255,255, 0.1);
+    background: rgba(255, 255, 255, 0.1);
+
+    ::v-deep .el-progress .el-progress-bar {
+      margin-bottom: 15px;
+    }
+
+    ::v-deep .el-progress-bar__inner {
+      background-color: rgba(60, 179, 113, 0.8);
+      top: 3px;
+      height: 65%;
+      text-align: center;
+      line-height: 0;
+      border: 1px solid black;
+      left: 3px;
+    }
+
+    ::v-deep .el-progress__text {
+      color: white !important;
+      font-size: 16px !important;
+      margin-left: 15px !important;
+      margin-bottom: 15px;
+    }
+  }
+
+  .skill-title {
+    font-weight: bold;
+    display: inline-block;
+    padding-bottom: 0.6rem;
   }
 
 }
